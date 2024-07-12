d37757d99a77f76819079a7537fd0101ee94cf4e
makaniko <109954583+makaniko@users.noreply.github.com>
2024-07-12 16:09:22 +0300

    Update NumberIndexesUtilTest.java

--- src/test/java/com/indexes/util/NumberIndexesUtilTest.java
+++ src/test/java/com/indexes/util/NumberIndexesUtilTest.java
@@ -13,8 +13,8 @@
 package com.indexes.util;
 
 import java.util.stream.Stream;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assert;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
