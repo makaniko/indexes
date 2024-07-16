8a675b74fee85602ed910ff9a536260322fb2d6c
makaniko <109954583+makaniko@users.noreply.github.com>
2024-07-16 15:51:22 +0300

    Update NumberIndexesUtilTest.java

--- src/test/java/com/indexes/util/NumberIndexesUtilTest.java
+++ src/test/java/com/indexes/util/NumberIndexesUtilTest.java
@@ -88,7 +88,10 @@ public class NumberIndexesUtilTest {
             .map(this::changeTypeToBigInt)
             .toArray(len -> new BigInteger[len][]);
         BigInteger[][] actualNumberIndexes = NumberIndexesUtil.convert(indexes);
-        Assertions.assertArrayEquals(expectedNumberIndexes, actualNumberIndexes);
+        Assertions.assertEquals(expectedNumberIndexes.length, actualNumberIndexes.length);
+        for (int i = 0; i < actualNumberIndexes.length; ++i) {
+            Assertions.assertArrayEquals(expectedNumberIndexes[i], actualNumberIndexes[i]);
+        }
     }
 
     public static IntFunction<int[][]> get2DimIntArrayGenerator() {
